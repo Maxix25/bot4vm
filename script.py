8cc1ad7d8942bcc92475a01dff517f28fe147f9e
Maxix25 <68287079+Maxix25@users.noreply.github.com>
2020-11-25 11:45:30 -0300

    Update script.py

--- script.py
+++ script.py
@@ -78,7 +78,8 @@ class Bot():
 
 	def type_cmd(self, command):
 		driver = self.driver
-		terminal = driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[3]/div[1]/div[2]/div')
+		#terminal = driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[3]/div[1]/div[2]/div')
+		terminal = driver.find_element_by_class_name("terminal xterm xterm-theme-default xterm-cursor-blink")
 		terminal.send_keys(command, Keys.RETURN)
 
 	def mission_abort(self):
