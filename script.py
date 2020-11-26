dbf0599410d87416f9974363c387baccb37710f5
Maxix25 <68287079+Maxix25@users.noreply.github.com>
2020-11-26 10:17:52 -0300

    Add comments and fix discord webhook

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+# Import selenium webdriver
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -9,12 +10,13 @@ import requests
 # from dotenv import load_dotenv
 # load_dotenv()
 
+# Some functions for logging and using the webhook from discord
 def log(text):
 	print("LOG:", text)
 	sys.stdout.flush()
 
 def webhook(body):
-	url = "https://discord.com/api/webhooks/780825549769015318/vcT3OpQRH2VOlI4fy9lD4kzGz_vn9D83LHMEKdn7bJ8L_z-wHc-nmu8TIYGv8-jv8aDa"
+	url = os.environ.get('WEBHOOK_URL')
 	values = {'username': 'VM Bot', 'content': body}
 	response = requests.post(url, data=values)
 	log(f"Webhook response={'false' if response.text else 'true'}")
@@ -36,17 +38,20 @@ class Bot():
 			error_count = 0
 
 			try:
+				# Firstly, get the goorm webpage loaded
 				driver.get("https://ide.goorm.io/")
 				time.sleep(3)
 				log("Logging in...")
 				print(driver.current_url)
 
 				#signin_btn = driver.find_element_by_xpath("//*[text()='Sign In']")
+				# Locate the sign in button and click it
 				signin_btn = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/header/nav/ul/li[1]/a")))
 				signin_btn.click()
 				print(driver.current_url)
 				time.sleep(5)
 				log("Sign in button located")
+				# Once in the login page, introduce email and password
 				email_field = driver.find_element_by_name("email")
 				password_field = driver.find_element_by_name("password")
 				log("Email field found successfully")
@@ -59,13 +64,14 @@ class Bot():
 				email_field.send_keys(Keys.RETURN)
 				log("Pressed enter")
 				time.sleep(5)
+				# If password and email are correct the bot logged in successfully
 				log("Bot logged in successfully!")
 				webhook("Bot logged in successfully!")
 				self.run_container()
 
 			except ValueError as e:
 				log(f"ERROR_MSG={e}")
-
+			# Once pressed the run button, press some buttons to not lose activity
 			webhook("Container is running!")
 			terminal = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH, "/html/body")))
 			while True:
@@ -78,6 +84,7 @@ class Bot():
 			driver.execute_script("window.scrollTo(0, 450)")
 			print(driver.current_url)
 			#run_btn = driver.find_elements_by_xpath("/html/body/div[1]/section[2]/div/section/div[2]/div[5]/div[2]/div/button")
+			# Locate the run button and click it, and switch to the tab it opens
 			run_btn = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/section[2]/div/section/div[2]/div[5]/div[2]/div/button')))
 			run_btn.click()
 			driver.switch_to.window(driver.window_handles[1])
