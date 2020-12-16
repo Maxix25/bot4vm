d893965232f64ed3da86ab1c7c696b44bc005157
Maxix25 <maximiliano.besoain@gmail.com>
2020-12-16 18:14:05 -0300

    Don't use webhook and use logs

--- script2.py
+++ script2.py
@@ -7,24 +7,20 @@ from helium import *
 def log(text):
 	print("LOG:", text)
 	sys.stdout.flush()
-
-def webhook(body):
-	url = os.environ.get("WEBHOOK_URL")
-	values = {'username': 'VM Bot', 'content': body}
-	response = requests.post(url, data = values)
-	log(f"Webhook response={'false' if response.text else 'true'}")
-
 start_chrome("ide.goorm.io", headless = True)
 click("Get Started")
 time.sleep(5)
 write(os.environ.get("EMAIL"))
+log("Bot wrote email")
 press(TAB)
+log("Bot pressed tab")
 write(os.environ.get("PASSWORD"))
+log("Bot wrote password")
 click("Login")
-webhook("Bot logged in!")
+log("Bot pressed Login")
 time.sleep(3)
 go_to("https://ide-run.goorm.io/workspace/MySql?language=us")
-webhook("Container is running!")
+log("Bot is running the container")
 time.sleep(10)
 while True:
 	write("ls")
