7ddadf546288eec046bf9b4c3aedc3a806ce9c22
Maxix25 <maximiliano.besoain@gmail.com>
2020-12-16 09:54:20 -0300

    Add log function and debug

--- script2.py
+++ script2.py
@@ -1,9 +1,15 @@
 import time
 import os
+import sys
 import requests
 from helium import *
+
+def log(text):
+	print("LOG:", text)
+	sys.stdout.flush()
+
 def webhook(body):
-	url = os.environ.get('WEBHOOK_URL')
+	url = os.environ.get("WEBHOOK_URL")
 	values = {'username': 'VM Bot', 'content': body}
 	response = requests.post(url, data=values)
 	log(f"Webhook response={'false' if response.text else 'true'}")
@@ -11,9 +17,9 @@ def webhook(body):
 start_chrome("ide.goorm.io", headless = True)
 click("Get Started")
 time.sleep(5)
-write(os.environ.get('EMAIL'))
+write(os.environ.get("EMAIL"))
 press(TAB)
-write(os.environ.get('PASSWORD'))
+write(os.environ.get("PASSWORD"))
 click("Login")
 webhook("Bot logged in!")
 time.sleep(3)
