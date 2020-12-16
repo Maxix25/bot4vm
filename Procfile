e2ab15c4d8aec52c4ff81486b79f4b44215524ae
Maxix25 <maximiliano.besoain@gmail.com>
2020-12-15 23:48:57 -0300

    Change script to helium and update Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python3 script.py
+worker: python3 script2.py
---
+++ script2.py
@@ -0,0 +1,25 @@
+import time
+import os
+from helium import *
+def webhook(body):
+	url = os.environ.get('WEBHOOK_URL')
+	values = {'username': 'VM Bot', 'content': body}
+	response = requests.post(url, data=values)
+	log(f"Webhook response={'false' if response.text else 'true'}"
+
+start_firefox("ide.goorm.io")
+click("Get Started")
+time.sleep(5)
+write(os.environ.get('EMAIL'))
+press(TAB)
+write(os.environ.get('PASSWORD'))
+click("Login")
+webhook("Bot logged in!")
+time.sleep(3)
+go_to("https://ide-run.goorm.io/workspace/MySql?language=us")
+webhook("Container is running!")
+time.sleep(10)
+while True:
+	write("ls")
+	press(ENTER)
+	time.sleep(10)
